2e8e3a20089a7f3f34691928d2fad96017271643
shaadrach <90820922+shaadrach@users.noreply.github.com>
2025-08-24 10:08:04 -0400

    update to website

--- Website/index.js
+++ Website/index.js
@@ -1,6 +1,6 @@
 const counter = document.getElementById("counter");
 // Paste your Lambda Function URL here
-const apiUrl = "https://leva5sv5ilw7j66mfs3p743q6m0cbihn.lambda-url.us-east-1.on.aws/";
+const apiUrl = "https://xzkysyzeq6sdztt273pdsfhowu0lhalv.lambda-url.us-east-1.on.aws/";
 async function updateCounter() {
   try {
     let response = await fetch(apiUrl);
